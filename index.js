1a5f1d9a4a6b599c8e574ca6c559bbd88f61e208
NDICHU <ndichumuriithi@gmail.com>
2022-03-31 15:46:11 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+const { VirtualConsole } = require("jsdom");
+
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase()
+}
+function logShout(string) {
+   // return string.toUpperCase();
+    
+    console.log(string.toUpperCase());
+}
+logShout("hello");
+
+function logWhisper(string){
+    
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if(string ===string.toLowerCase()){
+        return(`I can\'t hear you!` );
+    }
+    else if (string===string.toUpperCase()) {
+        return("YES INDEED!")
+    }
+    else if (string==="Let's have dinner together!") {
+        return("I would love to!")
+    }
+}
+sayHiToHeadphonedRoommate("HELLO")
+sayHiToHeadphonedRoommate("hello")
+sayHiToHeadphonedRoommate(`Let\'s have dinner together!`)
